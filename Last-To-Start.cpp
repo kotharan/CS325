52c3279b5443080cca34f462b5fd907d88e88a21
Anand Kothari <kotharan@oregonstate.edu>
2018-10-23 23:12:20 -0700

    Added the final code and comments for Last-To-Start

--- Last-To-Start.cpp
+++ Last-To-Start.cpp
@@ -1,3 +1,15 @@
+/* To compile and see the output run these commands sequentially ::
+ *
+ * 1) This will create an executable file named "a.exe"
+ *
+ *   g++ -std=c++11 Last-To-Start.cpp -o a.exe
+ *
+ *
+ * 2) Open the executable file with this command
+ *
+ *    .\a.exe
+ */
+
 #include <iostream>
 #include <algorithm>
 #include <fstream>
---
+++ ReadMe.txt
@@ -0,0 +1,10 @@
+To compile and see the output run these commands sequentially ::
+
+1) This will create an executable file named "a.exe"
+
+  g++ -std=c++11 Last-To-Start.cpp -o a.exe
+
+
+2) Open the executable file with this command
+
+   .\a.exe
