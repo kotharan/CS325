6a59d78075c50a33238d123f61dd655357555e76
Anand Kothari <kotharan@oregonstate.edu>
2018-10-23 22:49:07 -0700

    Added the final code for

--- Last-To-Start.cpp
+++ Last-To-Start.cpp
@@ -1,53 +1,100 @@
-
 #include <iostream>
-#include <vector>
+#include <algorithm>
 #include <fstream>
-
+#include <vector>
 
 using namespace std;
 
-ifstream infile("act.txt");
-vector<int> brr;
+struct activityInfo{
+int activityNum , start , finish; // Struct to store activity number , start time and end time
+};
 
-void lastToStart(int n)
+vector<activityInfo> ActivityList;
+vector<int> all_Activities;
+
+
+bool compare( activityInfo activity1,activityInfo activity2)
 {
-    int k = 0;
-   for( k = 0 ; k < n*n ; k++)
-   {
-        if(k%3==0)
-        {
-            cout<<brr[k];  // Prints the activity number [the first element of each activity]
+    if (activity1.finish < activity2.finish) return true;  // Used to sort as per activity end time
+    return false;
+}
 
-        }else{
-        continue;}
 
-   }
+void select_This_Activity(int num_Of_Activities)
+{
+    int i = 0,total_Act_Selected = 0;
+    total_Act_Selected++;               // Total count of the activities selected from all the activities provided in the act.txt file
+
+    // Select the first activity
+    all_Activities.resize(total_Act_Selected);
+    all_Activities[total_Act_Selected-1] = ActivityList[0].activityNum;
+
+    // Continue with other activities
+    for (int j = 1; j < num_Of_Activities; j++)
+    {
+                    /* If activity's start time greater than or
+                     equal to the finish time of previously selected
+                     activity, then select it */
+    if (ActivityList[j].start >= ActivityList[i].finish)
+    {
+    total_Act_Selected++;
+    all_Activities.resize(total_Act_Selected);
+    all_Activities[total_Act_Selected-1] = ActivityList[j].activityNum;
+    i = j;
+
+}}}
+
+void PrintOutput(int setNum)  // To print the output as per assignment instructions
+{
+    if(all_Activities[0] != 0)
+    {
+    int total_Act_Selected = all_Activities.size();
+    cout << "Set " << setNum << endl;
+    cout << "Number of activities selected = " << total_Act_Selected << endl;
+    cout << "Activities:";
+    for (int i = 0; i < total_Act_Selected; ++i)
+    {
+    cout << " " << all_Activities[i];
+    }
+    cout << endl<<endl;
+    }
 }
 
 int main()
 {
 
-
-
-    if(!infile)
+    string filename = "act.txt";
+    ifstream inFile(filename);
+    if (inFile.is_open())
     {
-        cout<< "File does not exist" <<endl;
+    int num_Of_Activities = 0; // Number of Activities
+    int setNum = 1; // for each set of activities
+
+
+    while(!inFile.eof())
+    {
+    ActivityList.resize(0);
+    inFile >> num_Of_Activities;
+    for (int i = 0; i < num_Of_Activities; ++i)
+    {
+    ActivityList.resize(i+1);
+    inFile >> ActivityList[i].activityNum;
+    inFile >> ActivityList[i].start;
+    inFile >> ActivityList[i].finish;
     }
 
-    ifstream file("act.txt");
+    sort(ActivityList.begin(), ActivityList.end(), compare);
 
-    int a;
-    while (file >> a)
-    {
-        brr.push_back(a);
+    select_This_Activity(num_Of_Activities);
+
+    PrintOutput(setNum++);
 
     }
-
-
-    int n =  brr[0];
-	brr.erase(brr.begin());
-
-    lastToStart(n);
-
-	return 0;
+    inFile.close();
+    }
+    else
+    {
+    cout << "File not found";
+    }
+    return 0;
 }
